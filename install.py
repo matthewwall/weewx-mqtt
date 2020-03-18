ac6454ee2c08d547bd85bb5206e0971447448c30
Matthew Wall <matthewwall@users.noreply.github.com>
2020-03-18 08:05:29 -0400

    bump version in the installer

--- install.py
+++ install.py
@@ -10,7 +10,7 @@ def loader():
 class MQTTInstaller(ExtensionInstaller):
     def __init__(self):
         super(MQTTInstaller, self).__init__(
-            version="0.20",
+            version="0.21",
             name='mqtt',
             description='Upload weather data to MQTT server.',
             author="Matthew Wall",
